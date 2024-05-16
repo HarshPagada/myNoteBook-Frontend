48a685e2cd8e1f65257264c4f1b84816be284a97
“harsh” <harshpagada@gmail.com>
2024-05-16 10:44:57 +0530

    first

--- Backend/db.js
+++ Backend/db.js
@@ -24,7 +24,7 @@ const mongoose = require('mongoose');
 
 connectMongo= async()=> {
   try {
-    await mongoose.connect('mongodb+srv://harshpagada:oCtzKF5pFc4wVCIV@cluster1.emrs1g7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1');
+    await mongoose.connect('https://mynotebook-backend-1-gegr.onrender.com');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
